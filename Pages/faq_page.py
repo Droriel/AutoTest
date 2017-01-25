ddc06378b41a274ffd4e24ce6df7232527b3974e
Droriel <izabela.rogos@gmail.com>
2017-01-25 15:55:47 +0100

    LoadingSimplePage - addig try and global variables

--- Pages/faq_page.py
+++ Pages/faq_page.py
@@ -4,7 +4,6 @@ from Pages.abstract_page import AbstractPage
 
 class FaqPage(AbstractPage):
     locators_list = {'FAQcontent': '//*[@id="article_10154_10826_23915_1.1"]//p'}
-    other_attributes_list = {'text': u'ź'}
 
     def __init__(self, driver):
         super().__init__(driver)
--- Tests/loading_simple_pages.py
+++ Tests/loading_simple_pages.py
@@ -13,6 +13,8 @@ from browser import Browser
 #W każdym pliku testu jedna główna klasa - musi mieć nazwę kończącą się na Test - potem tak zaczynają się z kolei nazwy metod
 
 class LoadingSimplePagesTest(unittest.TestCase):
+    other_attributes_list = {'text': u'ź'}
+
     def setUp(self):
         self.browser = Browser()
         self.driver = self.browser.start()
@@ -25,4 +27,4 @@ class LoadingSimplePagesTest(unittest.TestCase):
         # zawsze self.driver + czas
         WebDriverWait(self.driver,10).until(expected_conditions.presence_of_element_located((By.ID,'article_10154_10826_23915_1.1')))
         # WebDriverWait(self.driver, 10).until(expected_conditions.presence_of_element_located((By.XPATH, elId)))
-        page_faq.wait_for_text_in_element(page_faq.locators_list['FAQcontent'], page_faq.other_attributes_list['text'])
+        page_faq.wait_for_text_in_element(page_faq.locators_list['FAQcontent'], self.other_attributes_list['text'])
